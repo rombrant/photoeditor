34715e886d6463db51bf2f968e58c068c334dd2a
Roman Safin <gordonfreeman2332@gmail.com>
2020-08-11 00:51:16 +0300

    first commit

---
+++ precache-manifest.a947e49c7c2407f2d362515226044958.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91cd9cf61ad0ed6cf51e928984e7dea5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cc004644ef30d87d738",
+    "url": "/static/css/main.8779439d.chunk.css"
+  },
+  {
+    "revision": "e5b67d05b94e2e87797a",
+    "url": "/static/js/2.8a41b3a4.chunk.js"
+  },
+  {
+    "revision": "4fc1eca4ae67de9775d13aafa7463767",
+    "url": "/static/js/2.8a41b3a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cc004644ef30d87d738",
+    "url": "/static/js/main.06a68586.chunk.js"
+  },
+  {
+    "revision": "34c1e2afa4e2800fb839",
+    "url": "/static/js/runtime-main.36d29375.js"
+  },
+  {
+    "revision": "843c03ed6edd3f2a4dc35357feedbbec",
+    "url": "/static/media/icon-a.843c03ed.svg"
+  },
+  {
+    "revision": "d70663d06bd44437c178f522047470cc",
+    "url": "/static/media/icon-b.d70663d0.svg"
+  },
+  {
+    "revision": "6f440c0d2fa852de523edfd5383ad682",
+    "url": "/static/media/icon-c.6f440c0d.svg"
+  },
+  {
+    "revision": "4ef3abbd14fbe85ceebf36d900d2c9ad",
+    "url": "/static/media/icon-d.4ef3abbd.svg"
+  }
+]);
